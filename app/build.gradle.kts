411da666fe13ebd0205a1020645fad90afda70ee
farhannz <farhannz@protonmail.com>
2026-08-30 22:40:13 +0700

    chores

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,16 +55,6 @@ android {
         }
     }
 
-    externalNativeBuild {
-        cmake {
-            path = file("src/main/cpp/CMakeLists.txt")
-            version = "3.22.1"
-        }
-    }
-//
-//    ndk {
-//        abiFilters 'armeabi-v7a', 'arm64-v8a'
-//    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_21
         targetCompatibility = JavaVersion.VERSION_21
@@ -98,15 +88,14 @@ dependencies {
     implementation("androidx.room:room-ktx:$room")
     ksp("androidx.room:room-compiler:$room")
 
-    implementation(files("libs/PaddlePredictor.jar"))
-//    implementation(project(":opencv"))
     implementation("org.opencv:opencv:4.12.0")
     implementation("com.github.micycle1:Clipper2-java:1.3.1")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
     implementation("org.apache.lucene:lucene-analyzers-kuromoji:8.11.0")
-    implementation("com.microsoft.onnxruntime:onnxruntime-android-qnn:1.22.0")
-    implementation("ai.djl.huggingface:tokenizers:0.33.0") // HuggingFace tokenizers
+    implementation("com.microsoft.onnxruntime:onnxruntime-android-qnn:1.29.0")
+    //noinspection Aligned16KB
+    implementation("ai.djl.huggingface:tokenizers:0.36.0") // HuggingFace tokenizers
     //noinspection Aligned16KB
     implementation("ai.djl.android:tokenizer-native:0.33.0")
 
@@ -119,6 +108,7 @@ dependencies {
     testImplementation("edu.stanford.nlp:stanford-corenlp:4.5.1")
 //    testImplementation("edu.stanford.nlp:stanford-corenlp:4.5.1:models")
 //    testImplementation("edu.stanford.nlp:stanford-corenlp:4.5.1:pipeline")
+    testImplementation("org.openpnp:opencv:4.9.0-0")
     testImplementation("ai.djl.huggingface:tokenizers:0.33.0") // HuggingFace tokenizers
     testImplementation("com.microsoft.onnxruntime:onnxruntime:1.22.0")
     testImplementation(kotlin("test"))
